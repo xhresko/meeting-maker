9095f3fc800e1c204443d1db27ad6404c26790ba
Juraj Hresko <juraj.hresko@gmail.com>
2010-09-25 18:27:54 +0200

    null patch

--- arranger.pl
+++ arranger.pl
@@ -13,9 +13,22 @@ partner(4,2).
 room(1,2).
 room(2,5).
 
-% meeting(PartnerIds , Time)
+
+% request(Partners) - request to hold meeting between partners
+% Partners - list of partners Id
+request([1,2]).
+request([1,3,4]).
+request([3,2]).
+
+% meeting(PartnerIds , Time, Room)
 %meeting([1,2,3],2).
 %meeting(X,Time) :-
+meeting(Partners,Time,Room) :-
+    request(Partners),
+    room(Room,X),
+    persons_count(Partners,X).
+
+
 
 partners([]).
 partners([P|Rest]) :-
@@ -27,5 +40,9 @@ not_member(_,[]).
 not_member(Z,[X|Tail]) :-
     Z \= X,
     not_member(Z,Tail).
-    
+
+persons_count([],0).
+persons_count([_|Rest],Count) :-
+    persons_count(Rest,Y),
+    Count is Y+1.
 
